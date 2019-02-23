f48bab153ee23ea67d84ef8c3afb701bbebf806f
Takumi Karibe <takumi.k.5610@gmail.com>
2019-02-23 21:02:25 +0900

    #002 create to load YAML file

--- Package.swift
+++ Package.swift
@@ -14,7 +14,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Coupage",
-            dependencies: []),
+            dependencies: ["YamlLoader"]),
+        .target(
+            name: "YamlLoader",
+            dependencies: ["Yams"]),
         .testTarget(
             name: "CoupageTests",
             dependencies: ["Coupage"]),
---
+++ Sources/YamlLoader/YamlLoader.swift
@@ -0,0 +1,8 @@
+//
+//  YamlLoader.swift
+//  Coupage
+//
+//  Created by Takumi Karibe on 2019/02/23.
+//
+
+import Foundation
